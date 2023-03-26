ed75bacd1182459dfe0855865c31bea591719bba
Marrylin <marrylin2019@outlook.com>
2023-03-26 17:33:53 +0800

    add third party libs

---
+++ yolosegmention/libs/__init__.py
@@ -0,0 +1,11 @@
+"""
+This is a libs including YOLOv8 and QFluentWidgets
+
+@Project ：Graduation-Project-Fluent
+@File    ：__init__.py
+@Author  ：X0RB64
+@Date    ：2023/03/24 14:48
+"""
+
+from .qfluentwidgets import *
+from .ultralytics import YOLO
